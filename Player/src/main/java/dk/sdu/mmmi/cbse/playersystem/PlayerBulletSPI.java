d33c56291099c3414fe0ef69c022ef0517443e20
Gustav Skou Rasmussen <Gustav.skou.rasmussen@gmail.com>
2026-03-01 23:15:12 +0100

    player should implement its own createbullet

---
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerBulletSPI.java
@@ -0,0 +1,12 @@
+package dk.sdu.mmmi.cbse.playersystem;
+
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+
+public class PlayerBulletSPI implements BulletSPI {
+    @Override
+    public Entity createBullet(Entity shooter, GameData gameData) {
+
+    }
+}
